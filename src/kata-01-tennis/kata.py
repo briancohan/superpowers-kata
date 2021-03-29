181b0b5985618f897b1c9201850545a8894612cd
Brian Cohan <briancohan@gmail.com>
2021-03-28 20:48:12 -0400

    score announcements

--- src/kata-01-tennis/kata.py
+++ src/kata-01-tennis/kata.py
@@ -1,5 +1,12 @@
 import random
 
+SCORE = {
+    0: "Love",
+    1: "Fifteen",
+    2: "Thirty",
+    3: "Forty",
+}
+
 
 def point(weight: float = 0.5) -> int:
     """Determine which team wins the point.
@@ -11,3 +18,33 @@ def point(weight: float = 0.5) -> int:
         index of team that wins
     """
     return int(random.random() < weight)
+
+
+def announce(scores) -> str:
+    """Announce current score of the game.
+
+    :param scores:
+        List of two integers representing the number of points by each player.
+        Server score is listed first.
+    :returns:
+        String indicating score.
+    """
+    if scores[0] == scores[1]:
+        if scores[0] < max(SCORE):
+            return f"{SCORE[scores[0]]} All"
+        else:
+            return "Deuce"
+
+    if max(scores) <= max(SCORE):
+        return f"{SCORE[scores[0]]}-{SCORE[scores[1]]}"
+
+    if abs(scores[0] - scores[1]) == 1:
+        if scores[0] > scores[1]:
+            return "Advantage Server"
+        else:
+            return "Advantage Receiver"
+
+    if scores[0] > scores[1]:
+        return "Server Wins"
+    else:
+        return "Receiver Wins"
--- src/kata-01-tennis/test_kata.py
+++ src/kata-01-tennis/test_kata.py
@@ -1,3 +1,4 @@
+import pytest
 import kata
 
 
@@ -16,3 +17,36 @@ def test_point_with_weights():
         weight = i / 10
         avg = sum([kata.point(weight=weight) for _ in range(n)]) / n
         assert weight - eps < avg < weight + eps
+
+
+@pytest.mark.parametrize(
+    "scores, announcement",
+    [
+        ([0, 0], "Love All"),
+        ([0, 1], "Love-Fifteen"),
+        ([0, 2], "Love-Thirty"),
+        ([0, 3], "Love-Forty"),
+        ([1, 0], "Fifteen-Love"),
+        ([1, 1], "Fifteen All"),
+        ([1, 2], "Fifteen-Thirty"),
+        ([1, 3], "Fifteen-Forty"),
+        ([2, 0], "Thirty-Love"),
+        ([2, 1], "Thirty-Fifteen"),
+        ([2, 2], "Thirty All"),
+        ([2, 3], "Thirty-Forty"),
+        ([3, 0], "Forty-Love"),
+        ([3, 1], "Forty-Fifteen"),
+        ([3, 2], "Forty-Thirty"),
+        ([3, 3], "Deuce"),
+        ([4, 4], "Deuce"),
+        ([5, 4], "Advantage Server"),
+        ([6, 5], "Advantage Server"),
+        ([4, 5], "Advantage Receiver"),
+        ([5, 6], "Advantage Receiver"),
+        ([4, 0], "Server Wins"),
+        ([0, 4], "Receiver Wins"),
+    ],
+)
+def test_announce(scores, announcement):
+    """Test that the correct score is announced."""
+    assert kata.announce(scores) == announcement
